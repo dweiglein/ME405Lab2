99cd2f7a5cac48fdca3b41949bb504869cfb8913
tomhorizon <60243018+tomhorizon@users.noreply.github.com>
2023-02-02 14:24:07 -0800

    comment out read

--- PCSide.py
+++ PCSide.py
@@ -3,43 +3,48 @@ import serial
 from matplotlib import pyplot as plt
 
 
-parameter = b'500,50'    # setpoint and KP
+parameter = b'500,50\r\n'    # setpoint and KP
 
 # runs step response tests by sending characters through the USB serial port to the MicroPython board
 with serial.Serial('COM8', 115200) as s_port:
     s_port.write(parameter)  # Write bytes, not a string
 
+    # specify a long timeout
+
 # read the resulting data,
 # plot the step response with correctly labeled axes and title.
 
 # open the CSV file in read ('r') mode
-if 5 == 6:
-    with open('data.csv', 'r') as csv:
-        # initialize x and y
-        x_values = []
-        y_values = []
 
-        # try to read each line in the csv
-        for line in csv:
-            datapoint = line.strip().split(',')
+'''
+print(s_port.readline().split(b','))
+with serial.Serialopen('data.csv', 'r') as csv:
+    # initialize x and y
+    x_values = []
+    y_values = []
 
-            # try to convert each line into numbers
-            try:
-                x = float(datapoint[0])
-                y = float(datapoint[1])
+    # try to read each line in the csv
+    for line in csv:
+        datapoint = line.strip().split(',')
 
-                # only if both numbers are converted do they append
-                x_values.append(x)
-                y_values.append(y)
-            except ValueError:
-                pass
+        # try to convert each line into numbers
+        try:
+            x = float(datapoint[0])
+            y = float(datapoint[1])
 
-        # plot, label, and display - fun stuff found at:
-        plt.plot(x_values, y_values, 'k')
-        plt.minorticks_on()
-        plt.axhline(y=0, color='black', linewidth = .3)
-        plt.axvline(x=0, color='black', linewidth = .3)
-        plt.title("Position vs. Time", size=16)
-        plt.xlabel("Time, t [ms]")
-        plt.ylabel("Motor Position, x [encoder ticks]")
-        plt.show()
+            # only if both numbers are converted do they append
+            x_values.append(x)
+            y_values.append(y)
+        except ValueError:
+            pass
+
+    # plot, label, and display - fun stuff found at:
+    plt.plot(x_values, y_values, 'k')
+    plt.minorticks_on()
+    plt.axhline(y=0, color='black', linewidth = .3)
+    plt.axvline(x=0, color='black', linewidth = .3)
+    plt.title("Position vs. Time", size=16)
+    plt.xlabel("Time, t [ms]")
+    plt.ylabel("Motor Position, x [encoder ticks]")
+    plt.show()
+'''
