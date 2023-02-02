b7217c42b2c4b4b4339de8e2694b242555552c8f
jwfraserpoly <jwfraser@calpoly.edu>
2023-02-02 13:57:59 -0800

    up

--- lab2main.py
+++ lab2main.py
@@ -2,37 +2,42 @@
 # receive a setpoint
 # receive a Kp
 
-import pyb
-from encoder_reader import EncoderReader
-from motor_driver import MotorDriver
-
-# motor pins
-enablePin = pyb.Pin.board.PA10
-input1Pin = pyb.Pin.board.PB4
-input2Pin = pyb.Pin.board.PB5
-# encoder pins
-encoder1Pin = pyb.Pin.board.PB6
-encoder2Pin = pyb.Pin.board.PB7
-
-# set up timers
-motorTimer = pyb.Timer(3, freq=20000)
-encoderTimer = pyb.Timer(4, prescaler=0, period=0xFFFF)
-
-# initialize motor and timer
-motor1 = MotorDriver(enablePin, input1Pin, input2Pin, motorTimer)
-encoder1 = EncoderReader(encoder1Pin, encoder2Pin, 0, 0)
-
 # A constructor which sets the proportional gain, initial setpoint, and other necessary parameters.
 
 # A method run() which is called repeatedly to run the control algorithm.
+
 # This method should accept as parameters the setpoint and measured output.
 
-# It should return an actuation value which is sent to a motor in this lab but might be sent to another device
-# in another instance, as this is a generic controller.
+# It should return an actuation value which is sent to a motor in this lab but might be sent to another device in another instance, as this is a generic controller.
 
-# The run() method should not contain a loop and should not print the results of its running; those things go
-# in your main code.
+# The run() method should not contain a loop and should not print the results of its running; those things go in your main code.
 
 # A method set_setpoint() to set the setpoint.
 
 # A method set_Kp() to set the control gain.
+
+class Control:
+    def __init__(self, kp, setpt):
+        self.kp = kp
+        self.setpt = setpt
+
+    def run(self, pos):
+        error = self.setpt - pos
+        psi = self.kp*error
+        return psi
+
+    def set_setpoint(self, setpt_new):
+        self.setpt = setpt_new
+        return self.setpt
+
+    def set_Kp(self, kp_new):
+        self.kp = kp_new
+        print('kp is:', self.kp)
+        return self.kp
+    
+if __name__ == '__main__':
+    con = Control(4, 180)
+    con.set_Kp(5)
+    
+    
+    
